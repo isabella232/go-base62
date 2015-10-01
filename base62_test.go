51f845cb1ed43ad94a59c813ca2316a9c8bf32c0
Adam Dratwinski <adam.dratwinski@gmail.com>
2015-10-01 12:27:17 +0200

    Add support of uint64 values

--- base62_test.go
+++ base62_test.go
@@ -1,29 +1,32 @@
 package base62
 
 import (
-  "fmt"
-  "testing"
-  assert "github.com/pilu/miniassert"
+	"fmt"
+	assert "github.com/pilu/miniassert"
+	"math"
+	"testing"
 )
 
 func TestEncode(t *testing.T) {
-  assert.Equal(t, "0", Encode(0))
-  assert.Equal(t, "1B", Encode(99))
+	assert.Equal(t, "0", Encode(0))
+	assert.Equal(t, "1B", Encode(99))
+	assert.Equal(t, "lYGhA16ahyf", Encode(math.MaxUint64))
 }
 
 func TestDecode(t *testing.T) {
-  assert.Equal(t, 0,  Decode("0"))
-  assert.Equal(t, 99, Decode("1B"))
+	assert.Equal(t, 0, int(Decode("0")))
+	assert.Equal(t, 99, int(Decode("1B")))
+	assert.Equal(t, int64(-1), int64(Decode("lYGhA16ahyf")))
 }
 
 func ExampleEncode() {
-  fmt.Println(Encode(99))
-  // Output:
-  // 1B
+	fmt.Println(Encode(99))
+	// Output:
+	// 1B
 }
 
 func ExampleDecode() {
-  fmt.Println(Decode("1B"))
-  // Output:
-  // 99
+	fmt.Println(Decode("1B"))
+	// Output:
+	// 99
 }
